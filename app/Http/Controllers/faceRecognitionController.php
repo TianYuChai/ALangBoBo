bb7cda050ee6d8d82933480b7572dd55959f7e38
TianYuChai <641934743@qq.com>
2019-04-21 20:30:58 +0800

    update

--- app/Http/Controllers/faceRecognitionController.php
+++ app/Http/Controllers/faceRecognitionController.php
@@ -9,13 +9,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\home\BaseController;
 use Illuminate\Http\Request;
+use FileUpload;
 
 class faceRecognitionController extends BaseController
 {
     public function index(Request $request)
     {
         $face_img = $request->face_img;
-        $crid_img = base64EncodeImage($request->crid_img);
+        $crid_img = base64EncodeImage(FileUpload::url('image', $request->crid_img));
         dd($crid_img);
     }
 }
