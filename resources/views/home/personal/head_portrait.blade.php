f13894cd04bdee4ab6a552600e8817d0703e05ab
TianYuChai <641934743@qq.com>
2019-05-07 23:32:32 +0800

    个人中心-地址管理

--- resources/views/home/personal/head_portrait.blade.php
+++ resources/views/home/personal/head_portrait.blade.php
@@ -1,3 +1,5 @@
 <li>
-    <img src="{{ asset('home/images/img/shPerson.png.png') }}" alt=""/>
+    <img src="{{ !empty(auth()->guard('web')->user()->headimg) ?
+                            FileUpload::url('image', auth()->guard('web')->user()->headimg)
+                            : asset('home/images/img/shPerson.png.png') }}" alt="" style="width: 83px;height: 83px;"/>
 </li>
