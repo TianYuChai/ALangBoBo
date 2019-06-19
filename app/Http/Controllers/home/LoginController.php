1a6c2f83c2f392da8cb92f1b78ca1313cc8b64a7
TianYuChai <641934743@qq.com>
2019-06-19 15:09:13 +0800

    update

--- app/Http/Controllers/home/LoginController.php
+++ app/Http/Controllers/home/LoginController.php
@@ -33,7 +33,7 @@ class LoginController extends BaseController
         try{
             $loginService->dataFiltering($request);
             return $this->ajaxReturn([
-                'url' => !empty(session('url.intended')) ? session('url.intended') : route('personal.index'),
+                'url' => route('personal.index'),
                 'status' => 200
             ], 200);
         } catch (Exception $e) {
