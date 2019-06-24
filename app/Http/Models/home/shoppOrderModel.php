ee4ecf2e183af206ccd111d0eee9dbc2c00666dd
TianYuChai <641934743@qq.com>
2019-06-24 18:31:10 +0800

    update

--- app/Http/Models/home/shoppOrderModel.php
+++ app/Http/Models/home/shoppOrderModel.php
@@ -69,7 +69,7 @@ class shoppOrderModel extends Model
                                 'status' => 1003
                             ])->update(['status' => 1002]);
                             $item = GoodsModel::where('id', $query->sid)->sharedLock()->first();
-                            $item->sold = busub($item->sold, $query->num);
+                            $item->sold = bcsub()($item->sold, $query->num);
                             $item->save();
                             /*退款*/
                             if($query->getDirty()['status'] == 900) {
