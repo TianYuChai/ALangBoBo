da758556960261e340419503460981f064f68238
TianYuChai <641934743@qq.com>
2019-07-17 11:10:56 +0800

    update

--- app/Console/Commands/completeTimeout.php
+++ app/Console/Commands/completeTimeout.php
@@ -7,6 +7,8 @@
  */
 namespace App\Console\Commands;
 
+use App\Http\Models\currency\CapitalModel;
+use App\Http\Models\home\evaluationModel;
 use App\Http\Models\home\orderModel;
 use App\Http\Models\home\shoppOrderModel;
 use Illuminate\Console\Command;
@@ -34,6 +36,8 @@ class completeTimeout extends Command
         parent::__construct();
         $this->model = orderModel::class;
         $this->shoppOrderModel = shoppOrderModel::class;
+        $this->capitalModel = CapitalModel::class;
+        $this->evaluationModel = evaluationModel::class;
     }
 
     /**
@@ -42,9 +46,31 @@ class completeTimeout extends Command
     public function handle()
     {
         try {
-            $this->shoppOrderModel::where('status', 500)->where('signtime', '<', getTime())->update([
-               'status' => 600
-            ]);
+            $items = $this->shoppOrderModel::where('status', 500)->where('signtime', '<', getTime())->get();
+            foreach ($items as $item) {
+                $this->capitalModel::create([
+                    'uid' => $item->gid,
+                    'order_id' => $item->order_id,
+                    'money' => $item->satisfied_fees,
+                    'trade_mode' => isset($item->order) ? $item->order->pay_method : 'Alipay',
+                    'memo' => '满意度评价金额',
+                    'category' => 500,
+                    'g_order_id' => $item->id,
+                    'status' => 1001
+                ]);
+                $this->evaluationModel::create([
+                    'sid' => $item->sid,
+                    'uid' => $item->uid,
+                    'satisfaction' => 100,
+                    'goods_evaluation' => '自动评价',
+                    'service_evaluation' => '自动评价',
+                    'anonymous' => 1,
+                    'order_id' => $item->id,
+                    'images' => ''
+                ]);
+                $item->status = 600;
+                $item->save();
+            }
         } catch (Exception $e) {
             Log::info('订单签收后到时评价:', [
                 'time' => getTime(),
